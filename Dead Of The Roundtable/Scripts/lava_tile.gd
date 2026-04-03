92b267a7781c4bada86abf2404826ac0deac5125
Collin Longoria <collin.longoria@digipen.edu>
2026-04-02 17:15:44 -0700

    messing around with item cards

--- Dead Of The Roundtable/Scripts/lava_tile.gd
+++ Dead Of The Roundtable/Scripts/lava_tile.gd
@@ -20,11 +20,12 @@ func _on_detector_body_entered(body: Node3D) -> void:
 		if body not in victims:
 			victims.append(body)
 
-func _on_detector_body_exited(body: Node3D) -> void:
-	if body in victims:
-		victims.erase(body)
-
 func _on_damage_timer_timeout() -> void:
 	for victim in victims:
 		if is_instance_valid(victim) and victim.has_method("take_damage"):
 			victim.take_damage(DPS)
+
+
+func _on_detector_body_exited(body: Node3D) -> void:
+	if body in victims:
+		victims.erase(body)
